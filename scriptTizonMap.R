0e3a4b8f061c13b124a564f838cbfdec1b7a693d
jdcardenas10 <jd.cardenas10@uniandes.edu.co>
2020-04-30 15:44:44 -0500

    mapa proceso

--- scriptTizonMap.R
+++ scriptTizonMap.R
@@ -1,5 +1,24 @@
 setwd("/Users/josedanielcardenasrincon/Documents/map.agromakers/R-space")
 library(raster)
+library(sp)
 
-a=raster("TMED_C_01032020_fcst_02032020.tif")
-plot(a)
+list0 <- list.files(path="./Data/maps/geoTIFFhumedadhorario0104202000Z", full.names=TRUE)
+list1 <- list.files(path="./Data/maps/geoTIFFtemphorario0104202000Z",full.names=TRUE)
+imageRH <- stack(list0)
+imageTemp<-stack(list1)
+
+y0 <- extract(imageRH,c(1:ncell(imageRH)))
+coord <- xyFromCell(imageRH,1:ncell(imageRH))
+cbd0 <- cbind(coord,y0)
+rh_df <- as.data.frame(cbd0)
+head(rh_df)
+
+y1 <- extract(imageTemp,c(1:ncell(imageTemp)))
+coord <- xyFromCell(imageTemp,1:ncell(imageTemp))
+cbd1 <- cbind(coord,y1)
+temp_df <- as.data.frame(cbd1)
+head(temp_df)
+
+#count(ydf, vars = c("layer.1","layer.2","layer.3","layer.4"))
+#brk <- do.call(brick, lapply(list.files(path = "./Data/maps/geoTIFFhumedadhorario0104202000Z", pattern="RH*.*tif"), raster))
+
