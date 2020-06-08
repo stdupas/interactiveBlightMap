1e691c81edb662ae1906e7dd0b1bdab753a69aac
stdupas <dupas@legs.cnrs-gif.fr>
2020-06-08 12:47:08 -0500

    separated script in scriptTizonMap.R for getting and publishing blightR map and scriptTizonStation.R for getting blightR values from climatic station data and functions colled by the scripts. Separated function getandPlotBlightRMapFromDownloadedDate into blightRMapFromDownloadedDate obtain blightR raster, and function plotBlightMap to plot raster from raster argument by adding  Colombian border onto plot

--- scriptTizonMap.R
+++ scriptTizonMap.R
@@ -1,98 +1,18 @@
 setwd("/Users/josedanielcardenasrincon/Documents/Github/map.agromakers/R-space")
 # setwd("/home/dupas/map.agromakers/R-space/")
 
-library(raster)
-library(sp)
-library(chron)
 source("lateBlightMap.R")
+# Hay que descomentar la linea de abajo para que se pueda inicializar Shinny como aplicación web
+# y comentarla para la vista de script
 
-rastlist <- list.files(path = "./Data/maps/", all.files=TRUE, full.names=FALSE, patern='.tif$')
-list0 <- list.files(path="./Data/maps", "humedad", full.names=TRUE)
-list1 <- list.files(path="./Data/maps", "temp",full.names=TRUE)
-imageRH <- stack(list0)
-imageTemp<-stack(list1)
-RHtCstack
-getandplotBligthMap <- (imageRH,imageTemp,resistance)
+bligthMapS <- blightRMapFromDownloadedDate(resistance="S")
+plotBlightMap(blightMap = bligthMapS)
 
-Period 
+shinyApp(ui = ui, server = server)
 
-
-#y0 <- extract(imageRH,c(1:ncell(imageRH)))
-#coord <- xyFromCell(imageRH,1:ncell(imageRH))
-#cbd0 <- cbind(coord,y0)
-#rh_df <- as.data.frame(cbd0)
-
-#y1 <- extract(imageTemp,c(1:ncell(imageTemp)))
-#coord <- xyFromCell(imageTemp,1:ncell(imageTemp))
-#cbd1 <- cbind(coord,y1)
-#temp_df <- as.data.frame(cbd1)
-
-#df <- data.frame(matrix(ncol = 6, nrow = 0))
-#x <- c("stationID", "x", "y", "time", "temperature", "relativeHumidity")
-#colnames(df) <- x
-
-time <- chron(date="04/01/20", time="00:00:00")
-
-#for (i in 1:100) {
-#  for(j in (3:ncol(temp_df))){
-#  df[nrow(df) + 1,] <- c(i, temp_df[i,1], temp_df[i,2], time, temp_df[i,j], rh_df[i,j] )
-#  time=time+(1/24)
-#  }
-#}
-
-
-# ejemplo de mapa inventado (a stack of TRH folowed by TC)
-mapRHTC <- stack(
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 89.5,max = 93),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)),
-  raster(matrix (runif(n = 16,min = 7,max = 23),nrow = 4,ncol = 4)))
-
-
-result=getandplotBligthMap(mapRHTC)
-values(result)
-
+getandplotBligthMap <- (resistance="MS")
+getandplotBligthMap <- (resistance="R")
+col <- readOGR(dsn = "Data/maps/",layer = "COL_adm0")
+plot(getandplotBligthMap)
+plot(col,add=TRUE)
+list.files()
---
+++ scriptTizonStation.R
@@ -0,0 +1,4 @@
+setwd("/Users/josedanielcardenasrincon/Documents/Github/map.agromakers/R-space")
+# setwd("/home/dupas/map.agromakers/R-space/")
+
+source("lateBlightMap.R")
